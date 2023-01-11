62d1993d0d05a832bacbcc309ff8117ee7089b11
hakyung kim <96482380+hk9713@users.noreply.github.com>
2023-01-11 17:09:51 +0900

    [Day 002] 두 수의 나눗셈
    2023.01.11

---
+++ python/programmers/beginner/두 수의 나눗셈.py
@@ -0,0 +1,12 @@
+# 나의 풀이
+def solution(num1, num2):
+    return int(num1 / num2 * 1000)
+
+# 배운 풀이
+import math
+def solution(num1, num2):
+    return math.trunc(num1 / num2 *1000)
+
+'''
+math.trunc() - 반올림 하지 않고, 소수 부분만 제거한다
+'''
