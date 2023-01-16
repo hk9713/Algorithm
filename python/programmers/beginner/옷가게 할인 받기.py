2b6d27b1fd96b28e4e325186aae79c6dc39f6505
hk9713 <96482380+hk9713@users.noreply.github.com>
2023-01-16 12:17:57 +0900

    [Day 005] 옷가게 할인 받기
    2023.01.16

---
+++ python/programmers/beginner/옷가게 할인 받기.py
@@ -0,0 +1,17 @@
+# 나의 풀이
+def solution(price):
+    if price >= 500000:
+        return int(price*0.8)
+    elif price >= 300000:
+        return int(price*0.9)
+    elif price >= 100000:
+        return int(price*0.95)
+    else:
+        return price
+
+# 배운 풀이
+def solution(price):
+    discount_rates = {500000:0.8, 300000:0.9, 100000:0.95, 0:1}
+    for discount_price, discount_rate in discount_rates.items():
+        if price >= discount_price:
+            return int(price * discount_rate)
