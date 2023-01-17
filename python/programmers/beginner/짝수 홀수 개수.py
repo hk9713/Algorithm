ac8ef0c15709e6fabcf9196a745ad64f98fd2e2d
hakyung Kim <96482380+hk9713@users.noreply.github.com>
2023-01-17 23:15:10 +0900

    [Day 006] 짝수 홀수 개수
    2023.01.17

---
+++ python/programmers/beginner/짝수 홀수 개수.py
@@ -0,0 +1,16 @@
+# 나의 풀이
+def solution(num_list):
+    answer = [0,0]
+    for i in num_list:
+        if i%2==0:
+            answer[0] += 1
+        else:
+            answer[1] += 1
+    return answer
+
+# 배운 풀이
+def solution(num_list):
+    answer = [0,0]
+    for n in num_list:
+        answer[n%2]+=1
+    return answer
