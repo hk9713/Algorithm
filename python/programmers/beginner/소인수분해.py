8a168c884b0b0dd0e85e26cc69500fc4844a9e69
hk9713 <hk0713@nate.com>
2023-01-25 22:25:33 +0900

    [Day 012] 소인수분해
    2023.01.25

---
+++ python/programmers/beginner/소인수분해.py
@@ -0,0 +1,11 @@
+# 나의 풀이
+def solution(n):
+    answer = []
+    num = 2
+    while num <= n:
+        if n % num == 0:
+            answer.append(num)
+            n = n/num
+        else:
+            num += 1
+    return sorted(list(set(answer)))
