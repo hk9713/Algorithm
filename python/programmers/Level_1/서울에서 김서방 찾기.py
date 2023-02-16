b4a7358b381778109e2df034efe0a3b568249846
hk9713 <96482380+hk9713@users.noreply.github.com>
2023-02-16 12:38:43 +0900

    서울에서 김서방 찾기
    2023.02.16

---
+++ python/programmers/Level_1/서울에서 김서방 찾기.py
@@ -0,0 +1,8 @@
+# 나의 풀이
+def solution(seoul):    
+    point = seoul.index("Kim")
+    return f"김서방은 {point}에 있다"
+
+# 배운 풀이
+def findKim(seoul):
+    return "김서방은 {}에 있다".format(seoul.index('Kim'))
