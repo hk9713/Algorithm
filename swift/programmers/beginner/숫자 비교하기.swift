b8841084a668c8dbd5087b6a83a2f1eea78e0f5f
hk9713 <hk0713@nate.com>
2023-03-02 21:21:46 +0900

    숫자 비교하기
    2023.03.02

---
+++ swift/programmers/beginner/숫자 비교하기.swift
@@ -0,0 +1,14 @@
+// 나의 풀이
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    if num1 == num2{
+        return 1;
+    }
+    else{
+        return -1;
+    }
+}
+
+// 배운 풀이
+func solution(_ num1: Int, _ num2: Int) -> Int { num1 == num2 ? 1 : -1 }
